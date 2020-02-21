f837a34308f58fe851fc6fa6b6dbf6796b8995b0
nikitqa48 <Princen1k@yandex.ru>
2020-02-21 10:36:39 +0300

    https

--- django_api/settings.py
+++ django_api/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 # STATIC_ROOT = os.path.join('static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'backendinvest.admlr.lipetsk.ru/media/'
+MEDIA_URL = 'https://backendinvest.admlr.lipetsk.ru/admin/media/'
