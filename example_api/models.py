80c8b417f15470f3c6be7234628a619deebcfe55
nikitqa48 <Princen1k@yandex.ru>
2020-09-21 11:12:21 +0300

    db sync

--- example_api/models.py
+++ example_api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 from django.utils import timezone
 from django.urls import reverse
 from .choices import *
@@ -240,5 +240,3 @@ class Event(models.Model):
         verbose_name = 'Cобытие'
         verbose_name_plural = 'Событие'
 
-    def __str__(self):
-        return self.organisation 
